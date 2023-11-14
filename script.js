365c52b022c6b7abfad85f7ed1913119aac4f09a
atzuki <atzuki@protonmail.com>
2023-11-14 07:56:44 +0100

    displayed results

--- script.js
+++ script.js
@@ -1,63 +1,198 @@
-const canvas = document.getElementById('mapCanvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("mapCanvas");
+const ctx = canvas.getContext("2d");
+const switchMapButton = document.getElementById("switchMapButton");
+const metersSpan = document.getElementById("metersSpan");
+const milsSpan = document.getElementById("milsSpan");
+const angleSpan = document.getElementById("angleSpan");
+const conversionTable = [
+  [50, 1579],
+  [100, 1558],
+  [150, 1538],
+  [200, 1517],
+  [250, 1496],
+  [300, 1475],
+  [350, 1453],
+  [400, 1431],
+  [450, 1409],
+  [500, 1387],
+  [550, 1364],
+  [600, 1341],
+  [650, 1317],
+  [700, 1292],
+  [750, 1267],
+  [800, 1240],
+  [850, 1212],
+  [900, 1183],
+  [950, 1152],
+  [1000, 1118],
+  [1050, 1081],
+  [1100, 1039],
+  [1150, 988],
+  [1200, 918],
+  [1250, 800],
+];
+const maps = [
+  { name: "Fallujah", width: 3000, height: 3000 },
+  { name: "Fallujah", width: 1000, height: 1000 },
+];
+
+let currentMap = maps[0];
 
 const mapImage = new Image();
-mapImage.src = 'your-map-image.png';
+mapImage.src = `maps/${currentMap.name}.png`;
 
-let scale = 1;
+let scale = 0.5;
 let offsetX = 0;
 let offsetY = 0;
 let isDragging = false;
 let dragStartX = 0;
 let dragStartY = 0;
 
-function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+const MORTAR_MIN_PERIMETER = 50;
+const MORTAR_MAX_PERIMETER = 1250;
 
-	ctx.drawImage(mapImage, offsetX, offsetY, mapImage.width * scale, mapImage.height * scale);
+const circle = {};
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.drawImage(
+    mapImage,
+    offsetX,
+    offsetY,
+    currentMap.width * scale,
+    currentMap.height * scale
+  );
+
+  if (circle.x) {
+    drawCircle(MORTAR_MIN_PERIMETER, "rgba(255, 255, 255, 1)");
+    drawCircle(MORTAR_MAX_PERIMETER, "rgba(255, 255, 255, 0.4)");
+  }
+}
+
+function drawCircle(perimeter, color) {
+  ctx.beginPath();
+  ctx.arc(circle.x, circle.y, perimeter * scale, 0, 2 * Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+  ctx.stroke();
+}
+
+function drawDot(x, y, color) {
+  ctx.beginPath();
+  ctx.arc(x, y, 5, 0, 2 * Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+  ctx.stroke();
+}
+
+function drawText(meters, mils, angle) {
+  metersSpan.innerText = meters;
+  milsSpan.innerText = mils;
+  angleSpan.innerText = angle;
 }
 
 function handleMouseDown(e) {
-	isDragging = true;
-	dragStartX = e.clientX - offsetX;
-	dragStartY = e.clientY - offsetY;
+  if (e.button === 2) {
+    isDragging = true;
+    dragStartX = e.clientX - offsetX;
+    dragStartY = e.clientY - offsetY;
+  } else if (e.button === 0) {
+    draw();
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    if (circle.x) {
+      const distance = Math.sqrt(
+        (mouseX - circle.x) ** 2 + (mouseY - circle.y) ** 2
+      );
+      if (
+        distance >= MORTAR_MIN_PERIMETER * scale &&
+        distance <= MORTAR_MAX_PERIMETER * scale
+      ) {
+        const angle =
+          (Math.atan2(mouseY - circle.y, mouseX - circle.x) * (180 / Math.PI) +
+            450) %
+          360;
+        const distanceFromCenter = distance / scale;
+
+        drawDot(mouseX, mouseY, "red");
+        const miliradians = convertDistanceToMils(distanceFromCenter);
+
+        drawText(distanceFromCenter, miliradians, angle);
+      } else {
+        console.log(`Out of range!`);
+      }
+    } else {
+      circle.x = mouseX;
+      circle.y = mouseY;
+
+      draw();
+    }
+  }
 }
 
 function handleMouseUp() {
-	isDragging = false;
+  isDragging = false;
 }
 
 function handleMouseMove(e) {
-	if (isDragging) {
-		offsetX = e.clientX - dragStartX;
-		offsetY = e.clientY - dragStartY;
-		draw();
-	}
+  if (isDragging) {
+    offsetX = e.clientX - dragStartX;
+    offsetY = e.clientY - dragStartY;
+    draw();
+  }
 }
 
 function handleWheel(e) {
-	const delta = e.deltaY;
-	if (delta > 0) {
-		scale *= 1.1;
-	} else {
-		scale /= 1.1;
-	}
+  e.preventDefault();
 
-	draw();
+  const delta = e.deltaY;
+  if (delta < 0) {
+    scale *= 1.01;
+  } else {
+    scale /= 1.01;
+  }
+  draw();
 }
 
+function convertDistanceToMils(distance) {
+  for (let i = 0; i < conversionTable.length; ++i) {
+    if (conversionTable[i][0] == distance) return conversionTable[i][1];
+    if (conversionTable[i][0] > distance) {
+      const [x1, y1] = conversionTable[i - 1];
+      const [x2, y2] = conversionTable[i];
+      const t = (distance - x1) / (x2 - x1);
+      return y1 + t * (y2 - y1);
+    }
+  }
+}
+
+function changeMap() {
+  const selectedIndex = maps.indexOf(currentMap);
+  const nextIndex = (selectedIndex + 1) % maps.length;
+
+  currentMap = maps[nextIndex];
+  mapImage.src = `maps/${currentMap.name}.png`;
+
+  circle.x = 0;
+  circle.y = 0;
+
+  draw();
+}
 function init() {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-	mapImage.onload = () => {
-		draw();
-	};
+  mapImage.onload = () => {
+    draw();
+  };
 
-	canvas.addEventListener('mousedown', handleMouseDown);
-	canvas.addEventListener('mouseup', handleMouseUp);
-	canvas.addEventListener('mousemove', handleMouseMove);
-	canvas.addEventListener('wheel', handleWheel);
+  canvas.addEventListener("mousedown", handleMouseDown);
+  canvas.addEventListener("mouseup", handleMouseUp);
+  canvas.addEventListener("mousemove", handleMouseMove);
+  canvas.addEventListener("wheel", handleWheel);
+
+  switchMapButton.addEventListener("click", changeMap);
 }
 
 init();
